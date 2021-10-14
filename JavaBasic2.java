9636c40e8697dc93462dbd952aac7c5d47f72ba5
Nurdilla <nurdilladilla209@gmail.com>
2021-10-14 12:51:50 +0700

    initial commit

---
+++ JavaBasic2.java
@@ -0,0 +1,14 @@
+// tentang if else
+
+public class JavaBasic2 {
+    public static void main(String[]args){
+        String value ="A";
+
+        if(value == "B"){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        
+    }
+}
